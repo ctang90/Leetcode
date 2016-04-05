c721e0e80117e4571543b3fa1f1263a2fe4bd5e5
ctang90 <churutang@gmail.com>
2016-04-04 23:15:31 -0700

    [1. Two Sum][Wrong Answer]committed by LeetCode Extension

--- 1-Two-Sum/solution.py
+++ 1-Two-Sum/solution.py
@@ -8,11 +8,9 @@ class Solution(object):
         numDict = {}
         for i in range(0, len(nums)):
             numDict[nums[i]] = i
-        
-        for i in range(0, len(nums)):
             other = target - nums[i]
             if ( other in numDict.keys()) and (numDict[other] != i):
-                return [i, numDict[other]]
+                return [numDict[other], i]
                 
             
         
