4b312f66879b6cb7b6317ea8f396e163af7bcdcb
ctang90 <churutang@gmail.com>
2016-04-06 00:20:19 -0700

    [6. ZigZag Conversion][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 6-ZigZag-Conversion/solution.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        res = ""
+        for row in range(0, numRows):
+            rowRes = ""
+            index = row
+            while index < len(s):
+                rowRes = rowRes + s[index]
+                if row == 0 or row == numRows - 1:
+                    index = index + (2 * numRows - 2)
+                else:
+                    if len(rowRes) % 2 == 1:
+                        index = index + (2 * numRows - 2 - 2 * row)
+                    else:
+                        index = index + (2 * row)
+            res = res + rowRes
+                    
+        return res
+        
