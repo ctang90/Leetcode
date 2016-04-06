072b5f91df87ebd661239701109ee55eebeeb1aa
ctang90 <churutang@gmail.com>
2016-04-06 00:52:40 -0700

    [7. Reverse Integer][Wrong Answer]committed by LeetCode Extension

---
+++ 7-Reverse-Integer/solution.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        val = x
+        if (val == 0) or (val >= 2 ** 31 - 1) or (val < - 2 ** 31):
+            return 0
+        sign = (val < 0)
+        if val < 0:
+            val = -val
+        
+        # Ignore the 0s in the end
+        while val % 10 == 0:
+            val /= 10
+        
+        res = 0
+        while val > 0:
+            res = res * 10 + val % 10;
+            val /= 10
+            
+        if sign:
+            res = - res
+        return res
+            
+        
+            
+            
+        
+        
